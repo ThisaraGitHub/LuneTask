fb362a4a1be5025726a5aa01d60974e293e41d15
Thisara Sandaruwan <thisarasadaruwan92@gmail.com>
2022-01-09 12:06:21 +0530

    Main menu created with ui elements

--- Assets/sCRIPTS/LevelLoader.cs
+++ Assets/sCRIPTS/LevelLoader.cs
@@ -6,19 +6,19 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
-    public GameObject loadingScreen;
+
     public Slider slider;
     public Text progressText;
 
     public void LoadLevel(int sceneIndex)
     {
         StartCoroutine(LoadAsynchronously(sceneIndex));
+        slider.gameObject.SetActive(true);
     }
 
     IEnumerator LoadAsynchronously(int sceneIndex)
     {
         AsyncOperation oparation = SceneManager.LoadSceneAsync(sceneIndex);
-        loadingScreen.SetActive(false);
         while (!oparation.isDone)
         {
             float progress = Mathf.Clamp01(oparation.progress / 0.9f);
